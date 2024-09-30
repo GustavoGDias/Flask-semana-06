bc7dcf95549fe45c1d8fc5c64af1587eb247d409
GustavoGDias <gustavogoncalvesdias3@gmail.com>
2024-09-30 15:03:57 +0000

    first commit

--- hello.py
+++ hello.py
@@ -3,4 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return '<p>Hello from Flask!</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+    return '<p>Olá Alunos do IFSP!</p><table><tr><td><b>Aluno:</b></td><td>Gustavo Dias</td></tr><tr><td><b>Prontuário:</b></td><td>PT3026795</td></tr></table>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}</h1)'.format(name)
+
