afd3db1a9ab64d3380cb6ccff14566a3de735d58
GustavoGDias <gustavogoncalvesdias3@gmail.com>
2024-10-09 14:38:34 +0000

    update hello.py

--- hello.py
+++ hello.py
@@ -1,35 +1,38 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask, request, make_response, redirect, abort;
+from flask import Flask, request;
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1)';
+    return '''
+        <h1>Avaliação contínua: Aula 030</h1>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/user/Gustavo Gonçalves Dias/PT3026795/IFSP">Identificação</a></li>
+            <li><a href="/contextorequisicao">Contexto da requisição</a></li>
+        </ul>
+    '''
 
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}</h1)'.format(name)
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario, instituicao):
+    return '''
+        <h1>Avaliação contínua: Aula 030</h1>
+        <h2>Aluno: {}</h2>
+        <h2>Prontuário: {}</h2>
+        <h2>Instituição: {}</h2>
+        <p><a href="../../../">Voltar</a></p>
+    '''.format(name, prontuario, instituicao)
 
 @app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent');
-    return '<p>Your Browser is {}</p>' .format(user_agent);
-    
-@app.route('/codigostatusdiferente')
-def condigostatusdiferente():
-    return '<p>Bad request</p>', 400;
-    
-@app.route('/objetoresposta')
-def objetoresposta():
-    response = make_response('<h1>This document carries a cookie!</h1>');
-    response.set_cookie ('answer', '4.2');
-    return response
-
-@app.route('/redirecionamento')
-def redirecionamento ():
-    return redirect ('https://ptb.ifsp.edu.br');
-
-@app.route('/abortar')
-def abortar():
-    abort(404);
+def contextoRequisicao():
+    user_agent = request.headers.get('User-Agent')
+    ip_agent = request.remote_addr
+    base_url = request.host
+    return '''
+        <h1>Avaliação contínua: Aula 030</h1>
+        <h2>Seu navegador é: {}</h2>
+        <h2>O IP do computador remoto é: {}</h2>
+        <h2>O host da aplicação é: {}</h2>
+        <p><a href="./">Voltar</a></p>
+    '''.format(user_agent, ip_agent, base_url)
