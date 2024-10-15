ddbd59388893b19d1682062eb766f1bcaf1c2cbb
GustavoGDias <142761074+GustavoGDias@users.noreply.github.com>
2024-10-15 14:40:29 -0300

    update hello.py

--- hello.py
+++ hello.py
@@ -1,38 +1,25 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask, request;
+from datetime import datetime
+from Flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
-    return '''
-        <h1>Avaliação contínua: Aula 030</h1>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/user/Gustavo Gonçalves Dias/PT3026795/IFSP">Identificação</a></li>
-            <li><a href="/contextorequisicao">Contexto da requisição</a></li>
-        </ul>
-    '''
+    return render_template('index.html', current_time=datetime.utcnow())
 
-
-@app.route('/user/<name>/<prontuario>/<instituicao>')
-def user(name, prontuario, instituicao):
-    return '''
-        <h1>Avaliação contínua: Aula 030</h1>
-        <h2>Aluno: {}</h2>
-        <h2>Prontuário: {}</h2>
-        <h2>Instituição: {}</h2>
-        <p><a href="../../../">Voltar</a></p>
-    '''.format(name, prontuario, instituicao)
-
-@app.route('/contextorequisicao')
-def contextoRequisicao():
-    user_agent = request.headers.get('User-Agent')
-    ip_agent = request.remote_addr
-    base_url = request.host
-    return '''
-        <h1>Avaliação contínua: Aula 030</h1>
-        <h2>Seu navegador é: {}</h2>
-        <h2>O IP do computador remoto é: {}</h2>
-        <h2>O host da aplicação é: {}</h2>
-        <p><a href="./">Voltar</a></p>
-    '''.format(user_agent, ip_agent, base_url)
+@app.route('/user/<name>')
+def name(name):
+    return render_template('user.html', name=name)
