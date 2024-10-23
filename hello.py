c9580608adec298e12bd6384084cc723265b61fe
GustavoGDias <142761074+GustavoGDias@users.noreply.github.com>
2024-10-23 19:19:02 -0300

    update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -14,9 +14,9 @@ moment = Moment(app)
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def name(name):
-    return render_template('user.html', name=name)
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario, instituicao):
+    return render_template('user.html', name=name, prontuario=prontuario, instituicao=instituicao)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -25,3 +25,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
  return render_template('500.html'), 500
+
+ @app.route('/contextorequisicao/<name>')
+def contextorequisicao(name):
+    navegador=request.headers.get('User-Agent')
+    ip_agent=request.remote_addr
+    base_url=request.host
+    return render_template('contextorequisicao.html', name=name, navegador=navegador, ip_agent=ip_agent, base_url=base_url)
