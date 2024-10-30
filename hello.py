2782f5c453574cf2edae39915861521e2a2a0f15
GustavoGDias <gustavogoncalvesdias3@gmail.com>
2024-10-30 18:31:38 +0000

    update hello.py and templates

--- hello.py
+++ hello.py
@@ -1,22 +1,45 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators= [DataRequired()])
+    submit = SubmitField('Submit')
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Chave Secreta'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>/<prontuario>/<instituicao>')
 def user(name, prontuario, instituicao):
     return render_template('user.html', name=name, prontuario=prontuario, instituicao=instituicao)
+
+@app.route('/contextorequisicao/<name>')
+def contextorequisicao(name):
+    navegador=request.headers.get('User-Agent')
+    ip_agent=request.remote_addr
+    base_url=request.host
+    return render_template('contextorequisicao.html', name=name, navegador=navegador, ip_agent=ip_agent, base_url=base_url)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -25,10 +48,3 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
  return render_template('500.html'), 500
-
- @app.route('/contextorequisicao/<name>')
-def contextorequisicao(name):
-    navegador=request.headers.get('User-Agent')
-    ip_agent=request.remote_addr
-    base_url=request.host
-    return render_template('contextorequisicao.html', name=name, navegador=navegador, ip_agent=ip_agent, base_url=base_url)
