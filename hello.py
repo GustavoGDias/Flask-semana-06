f86c0862dd1132595f10962169257a3aa1324e52
GustavoGDias <gustavogoncalvesdias3@gmail.com>
2024-11-06 13:16:21 +0000

    update hello.py

--- hello.py
+++ hello.py
@@ -7,28 +7,39 @@ from flask_moment import Moment
 from datetime import datetime
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators= [DataRequired()])
-    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Chave Secreta'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class StudentForm(FlaskForm):
+    name = StringField('Informe o seu nome', validators= [DataRequired()])
+    surname = StringField('Informe o seu sobrenome:', validators= [DataRequired()])
+    instituicao = StringField('Informe a sua Instituição de ensino:', validators= [DataRequired()])
+    disciplina = SelectField('Informe a sua disciplina:', choices=[('DSWA5', 'DSWA5'), ('DWBA4', 'DWBA4'), ('Gestão de projetos', 'Gestão de projetos')], validators= [DataRequired()])
+    submit = SubmitField('Submit')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
+    form = StudentForm()
+    ip_agent=request.remote_addr
+    base_url=request.host
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+            flash('Você alterou o seu nome!')
         session['name'] = form.name.data
+        session['surname'] = form.surname.data
+        session['instituicao'] = form.instituicao.data
+        session['disciplina'] = form.disciplina.data
+        session['ip_agent'] = ip_agent
+        session['base_url'] = base_url
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), surname=session.get('surname'), instituicao=session.get('instituicao'), disciplina=session.get('disciplina'), ip_agent=session.get('ip_agent'), base_url=session.get('base_url'), current_time=datetime.utcnow())
 
 @app.route('/user/<name>/<prontuario>/<instituicao>')
 def user(name, prontuario, instituicao):
